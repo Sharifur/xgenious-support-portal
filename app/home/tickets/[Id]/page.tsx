749a5d49ec7c28366f0644a6019479c4c683bb82
sharifur <rsharifur824@gmail.com>
2023-12-09 23:39:18 +0600

    wip

--- app/home/tickets/[Id]/page.tsx
+++ app/home/tickets/[Id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Layout, Menu, theme } from 'antd';
+import { Alert, Avatar, Layout, Menu, theme } from 'antd';
+import Sider from 'antd/es/layout/Sider';
 const {Content } = Layout;
 
 const TicketDetails = () => {
@@ -10,7 +11,25 @@ const TicketDetails = () => {
     return ( 
         <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
             <div style={{ padding: 24, background: colorBgContainer }}>
-                Ticket Details
+                <span className="text-gray-500 mr-4 font-medium block text-2xl">#12345</span>
+                <h4 className="font-bold text-black text-2xl mb-6">
+                    Show notification if today is Friday/Saturday that we are on holiday
+                </h4>
+                <div className={"flex flex-row justify-between"}>
+                    <div className='bg-slate-50 w-3/5 p-6'>
+                        <div className='mb-6'>
+                            <Alert  message="This ticket has been closed two" showIcon type="warning" />
+                        </div>
+                        ticket details
+                    </div>
+                    <div className='bg-slate-50 w-2/5 p-6 border-l'>
+                        <h3 className='text-xl mb-4'>Agent working on this tickets</h3>
+                        <div className='flex flex-row items-center gap-2'>
+                            <Avatar>U</Avatar>
+                            <span className='font-medium text-sm'>Sharifur Rahman</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </Content>
      );
